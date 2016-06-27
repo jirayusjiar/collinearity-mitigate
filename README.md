0c4cffbd413695e8540d79ee7d596cca74a0a097
Jirayus Jiarpakdee <jiyoyo@hotmail.com>
2016-06-27 17:06:30 +0900

    Base code and import dataset DONE

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # collinearity-mitigate
+
+Approaches to mitigate collinearity
+- Iteratively remove highly correlated variables
+- Ridge Regression
+- PCA
+- Redun
+- Wrapper
---
+++ collinearityMitigation.R
@@ -0,0 +1,42 @@
+### Mitigate collinearity in defect dataset
+
+## Set working directory
+script.dir <- dirname(sys.frame(1)$ofile)
+setwd(script.dir)
+
+## Import packages 
+# These two lines are used to download DefectData library
+#library(devtools)
+#install_github("klainfo/DefectData")
+library(DefectData)
+library(compiler)
+
+enableJIT(3)
+
+## Define global variables and functions
+
+
+
+## Main
+
+for(targetProjectId in 1:nrow(listData)) {
+  
+  print(paste('ProjectID:', targetProjectId,"START"))
+  datasetInfo <<- c(targetProjectId)  
+  
+  targetData <- as.character(listData[targetProjectId, 1])
+  datasetInfo <<- c(datasetInfo,targetData)
+  
+  # Load data
+  Data <- loadData(targetData)
+  
+  # retrieve data, name of dependent and independent variables
+  dataset <- Data$data
+  dep <- Data$dep
+  indep <- Data$indep
+  # Finish import dataset
+  
+  # Mitigate collinearity 
+  
+  
+}
