97733ab3060e9d32909404e3c26ae67ae42f1015
Jirayus Jiarpakdee <jiyoyo@hotmail.com>
2016-07-05 11:09:08 +0900

    PCA Implementation DONE

--- collinearityMitigation.R
+++ collinearityMitigation.R
@@ -1,4 +1,6 @@
 ### Mitigate collinearity in defect dataset
+### PCA approach
+### Ref: http://www.r-bloggers.com/computing-and-visualizing-pca-in-r/
 
 ## Set working directory
 script.dir <- dirname(sys.frame(1)$ofile)
@@ -10,6 +12,7 @@ setwd(script.dir)
 #install_github("klainfo/DefectData")
 library(DefectData)
 library(compiler)
+library(stats)
 
 enableJIT(3)
 
@@ -34,9 +37,20 @@ for(targetProjectId in 1:nrow(listData)) {
   dataset <- Data$data
   dep <- Data$dep
   indep <- Data$indep
+  datasetLog <- dataset
+  datasetLog[indep] <- log1p(datasetLog[indep])
   # Finish import dataset
   
-  # Mitigate collinearity 
-  
-  
+  ## Mitigate collinearity - PCA
+  # Calculate PCA
+  dataset.pca <- prcomp(dataset[indep],
+                        center=TRUE,
+                        scale=TRUE)
+  # Intepretation
+  # http://webspace.ship.edu/pgmarr/Geo441/Lectures/Lec%2017%20-%20Principal%20Component%20Analysis.pdf
+  print(dataset.pca)
+  plot(dataset.pca,type="l")
+  summary(dataset.pca)
+  dataset.pca$rotation # Rotated component matrix
+  corrplot(cor(dataset[indep],dataset.pca$x),method="number") #corrGraph between original and PCA
 }
